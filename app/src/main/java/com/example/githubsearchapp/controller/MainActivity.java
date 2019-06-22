1589d6d6f864f500e2480eb2c85a3b3b7d0cfd61
Stanislav Humeniuk <whoamidarkknight@gmail.com>
2019-06-23 02:21:09 +0300

    create layout for repository and his subscribers

--- app/src/main/java/com/example/githubsearchapp/controller/MainActivity.java
+++ app/src/main/java/com/example/githubsearchapp/controller/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.githubsearchapp;
+package com.example.githubsearchapp.controller;
+
+import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.githubsearchapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
