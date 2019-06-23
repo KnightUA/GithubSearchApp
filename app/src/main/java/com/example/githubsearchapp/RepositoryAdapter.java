d89dc199ffc10c247cfb4aa2be0eba8230d9b766
Stanislav Humeniuk <whoamidarkknight@gmail.com>
2019-06-23 13:47:26 +0300

    List adapter with getting json info from api

---
+++ app/src/main/java/com/example/githubsearchapp/RepositoryAdapter.java
@@ -0,0 +1,87 @@
+package com.example.githubsearchapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.githubsearchapp.model.Repository;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class RepositoryAdapter extends RecyclerView.Adapter<RepositoryAdapter.ViewHolder> {
+
+    private List<Repository> repositories;
+    private Context context;
+
+    public RepositoryAdapter(Context context, List<Repository> repositories) {
+
+        this.context = context;
+        this.repositories = repositories;
+    }
+
+    @NonNull
+    @Override
+    public RepositoryAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.repository_item_list, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RepositoryAdapter.ViewHolder holder, int position) {
+
+        holder.repositoryName.setText(repositories.get(position).getName());
+        holder.description.setText(repositories.get(position).getDescription());
+        holder.forkCount.setText(repositories.get(position).getForkCount());
+
+        Picasso.get()
+                .load(repositories.get(position).getOwner().getAvatarUrl())
+                .placeholder(R.drawable.ic_default_image)
+                .into(holder.ownerAvatar);
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return repositories.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView repositoryName;
+        private TextView description;
+        private TextView forkCount;
+        private ImageView ownerAvatar;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            repositoryName = (TextView) itemView.findViewById(R.id.name);
+            description = (TextView) itemView.findViewById(R.id.description);
+            forkCount = (TextView) itemView.findViewById(R.id.numberOfForks);
+            ownerAvatar = (ImageView) itemView.findViewById(R.id.avatarImg);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int pos = getAdapterPosition();
+                    if (pos != RecyclerView.NO_POSITION) {
+
+                        /*TODO Add intent to another screen with values of subscribers*/
+
+                        Repository clickedData = repositories.get(pos);
+                        Toast.makeText(view.getContext(), "You clicked " + clickedData.getName(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+}
--- app/src/main/java/com/example/githubsearchapp/controller/MainActivity.java
+++ app/src/main/java/com/example/githubsearchapp/controller/MainActivity.java
@@ -1,16 +1,91 @@
 package com.example.githubsearchapp.controller;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.example.githubsearchapp.R;
+import com.example.githubsearchapp.RepositoryAdapter;
+import com.example.githubsearchapp.api.JSONPlaceHolderApi;
+import com.example.githubsearchapp.api.NetworkService;
+import com.example.githubsearchapp.model.Repository;
+import com.example.githubsearchapp.model.RepositoryResponse;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private ProgressDialog progressDialog;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefresh);
+
+        swipeRefreshLayout.setColorSchemeResources(android.R.color.holo_blue_light);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                loadJSON();
+                Toast.makeText(MainActivity.this, "Github Repositories Refreshed", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void initViews() {
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Fetching Github Repositories ...");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        recyclerView.smoothScrollToPosition(0);
+        loadJSON();
+    }
+
+    private void loadJSON() {
+        try {
+            JSONPlaceHolderApi apiService = NetworkService.getInstance().getJSONApi();
+            Call<RepositoryResponse> call = apiService.getRepositories();
+            call.enqueue(new Callback<RepositoryResponse>() {
+                @Override
+                public void onResponse(Call<RepositoryResponse> call, Response<RepositoryResponse> response) {
+                    List<Repository> repositories = response.body().getRepositories();
+                    recyclerView.setAdapter(new RepositoryAdapter(getApplicationContext(), repositories));
+                    recyclerView.smoothScrollToPosition(0);
+                    swipeRefreshLayout.setRefreshing(false);
+                    progressDialog.hide();
+                }
+
+                @Override
+                public void onFailure(Call<RepositoryResponse> call, Throwable t) {
+                    Log.d("Error", t.getMessage());
+                    Snackbar.make(swipeRefreshLayout, "Error: Fetching Data!", Snackbar.LENGTH_SHORT).show();
+                    swipeRefreshLayout.setRefreshing(false);
+                    progressDialog.hide();
+                }
+            });
+        } catch (Exception e) {
+            Log.d("Error", e.getMessage());
+            Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
